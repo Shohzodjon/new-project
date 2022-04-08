561b1666fc59b8214d22862bfe966b7570eb2b42
Shohzod <xorozovshohzod@gmail.com>
2022-04-08 16:36:27 +0300

    add react--spinner

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
@@ -6,18 +7,33 @@ import Login from "./components/login/Login";
 import MainComponent from "./components/main-component/MainComponet";
 import SignUp from "./components/sign-up/SignUp";
 
+import { PacmanLoader } from "react-spinners";
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/maincomponent" element={<MainComponent />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {loading ? (
+        <div className="loader--page">
+          <PacmanLoader color={"#6CF5DA"} loading={loading} size={80} />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/maincomponent" element={<MainComponent />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      )}
     </div>
   );
 }
